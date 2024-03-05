25ddf37f5d522798797592eb2994d99a1d6b695a
BorisMiasnikov <147999181+BorisMiasnikov@users.noreply.github.com>
2024-03-05 19:28:34 +0300

    Delete NewsPaper directory

--- NewsPaper/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-# Импортируем созданное нами представление
-from .views import PostsList, PostDetail
-
-urlpatterns = [
-    path('', PostsList.as_view()),
-    path('<id>', PostDetail.as_view()),
-]
