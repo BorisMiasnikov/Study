12a033d2c440865a80c6e348ee896716def00f13
BorisMiasnikov <147999181+BorisMiasnikov@users.noreply.github.com>
2024-03-09 19:13:25 +0300

    Delete NewsPaper directory

--- NewsPaper/news/templatetags/date_tags.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from django import template
-
-register = template.Library()
-
-@register.simple_tag()
-def current_time(format_string = '%b %d %Y %a'):
-    return datetime.utcnow().strftime(format_string)
